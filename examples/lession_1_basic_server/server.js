94f95a21f097b13e846d6cf68113843d0f42da60
Ravindranath <ravindranath@Ravindranaths-MacBook-Air.local>
2017-01-03 10:50:03 +0530

    some more

--- examples/lession_1_basic_server/server.js
+++ examples/lession_1_basic_server/server.js
@@ -1,28 +1,64 @@
-var Hapi = require('hapi')
+var Hapi = require('hapi');
 
 // create new server instance
-var server = new Hapi.Server()
+var server = new Hapi.Server();
 
 // add server’s connection information
 server.connection({
   host: 'localhost',
   port: 3000
-})
+});
 
-// add “hello world” route
+// add basic route
 server.route({
   method: 'GET',
   path: '/',
   handler: function (request, reply) {
-    reply('Hello World!')
+    reply('Hello World!');
   }
-})
+});
 
+// add home route
+server.route({
+  method: 'GET',
+  path: '/home',
+  handler: function (request, reply) {
+    reply('Hello Home!');
+  }
+});
+
+//add param route
+server.route({
+  method: 'GET',
+  path: '/hello/{username}',
+  handler: function (request, reply) {
+     // Never forget to encode the values coming from the client
+     reply('Hello, ' + encodeURIComponent(request.params.username) + '!');
+  }
+});
+
+//add optional param route
+server.route({
+  method: 'GET',
+  path: '/hi/{username?}',
+  handler: function (request, reply) {
+     var name = (request.params.username) ?
+          encodeURIComponent(request.params.username) : 'stranger';
+     reply('Hi, ' + name + '!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: function(request, reply) {
+    reply("other route");
+  }
+});
 // start your server
 server.start(function (err) {
   if (err) {
-    throw err
+    throw err;
   }
-
-  console.log('Server running at: ' + server.info.uri)
-})
+  console.log('Server running at: ' + server.info.uri);
+});
